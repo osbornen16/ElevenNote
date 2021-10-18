11f8434ce5a28ab9a5f32d0507c64458a4bfaf7a
Nate Osborne <osbornen16@gmail.com>
2021-10-18 14:38:53 -0400

    post lunch

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -18,7 +18,7 @@ namespace ElevenNote.Data
         [Required]
         public string Content { get; set; }
         [Required]
-        public DateTimeOffset CreateUtc { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
